47bb8e362a3ee623b12745f2c1a017f2f5ab3646
saxenaharsh <harsh.saxena@gmail.com>
2020-06-02 19:21:47 +0530

    Add files via upload

---
+++ Person/src/com/HarshSaxena/Person.java
@@ -0,0 +1,54 @@
+package com.HarshSaxena;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public String getLastName(){
+        return this.lastName;
+    }
+    public void setAge(int age){
+        if(age < 0 || age > 100){
+            this.age = 0;
+
+        }else{
+            this.age = age;
+        }
+
+    }
+    public int getAge(){
+        return this.age;
+    }
+
+    public boolean isTeen(){
+        if (this.age > 12 && this.age < 20){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public String getFullName(){
+        if(this.firstName.isEmpty() == true && this.lastName.isEmpty() == true){
+            return "";
+        }else if(this.firstName.isEmpty() == true){
+            return this.lastName;
+        }else if(this.lastName.isEmpty()){
+            return this.firstName;
+        }else{
+            return this.firstName + " " + this.lastName;
+        }
+    }
+
+}
